754a9bcbe267e84d4aef88dcc8a41c033fe42f46
Ivanuch22 <69019482+Ivanuch22@users.noreply.github.com>
2022-01-31 20:17:20 +0200

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,13 @@
+const swiper = new Swiper('.swiper', {
+    // Optional parameters
+    direction: 'horizontal',
+    slidesPerView: 1,
+    loop: true,
+    spaceBetween: 84,
+    // Navigation arrows
+    navigation: {
+      nextEl: '.swiper-buttonS-next',
+      prevEl: '.swiper-buttonS-prev',
+    },
+  
+  });
